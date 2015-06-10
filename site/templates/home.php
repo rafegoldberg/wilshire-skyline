03adb65ee23101de7fb85e512a4a4c6796236267
Rafe Goldberg <rafegoldberg@gmail.com>
2015-06-10 10:33:10 -0400

    Simplify templates

--- site/templates/home.php
+++ site/templates/home.php
@@ -2,12 +2,14 @@
 
 	<? snippet('header') ?>
 	<? snippet('menu')   ?>
+ 
 	<main class="main" role="main">
 		<div class="text">
-			<h1><?= $page->title()->html() ?></h1>
+			<h3><?= $page->title()->html() ?></h3>
 			<?= $page->text()->kirbytext() ?>
 		</div>
 	</main>
+
 	<? snippet('footer') ?>
 
 <? snippet('bottom') ?>

61418b2ca041edfcc3c0a47fcc2a798cd26c1f9f
Rafe Goldberg <rafegoldberg@gmail.com>
2015-06-14 16:11:16 -0400

    Comment meta/per; add favicon

--- site/snippets/meta/pre.php
+++ site/snippets/meta/pre.php
@@ -2,13 +2,26 @@
 <html lang="en">
 <head>
 
+	<!--| Meta Definitions
+	|–––| 
+	|–-->
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<? snippet('meta/top.data') ?>
 
+	<!--| Scripts + Stylesheets
+	|–––| Link main site stylesheets;
+	|–––| initialize scripts + sheets
+	|–––| post-lib $GLOBALS variable,
+	|–––| for use see `meta/post.php`
+	|–-->
 	<?= css('assets/css/grid.css') ?>
 	<?= css('assets/css/main.css') ?>
-	<? init_libs() ?>
+	<? init_libs() //@depends[snippet/meta.post] ?>
+
+	<!--| Favicon
+	|–-->
+	<link rel="icon" href="favicon.ico" type="image/x-icon" />
 
 </head>
 <body class='grid-9 grid-t-10 grid-m-12 centered<?= isset($body)?$body:'' ?>'>

21e35d97c25a50a567678f83269277a3859bfeca
Rafe Goldberg <rafegoldberg@gmail.com>
2015-06-15 12:36:41 -0400

    Correct field order of b/site

--- site/blueprints/site.php
+++ site/blueprints/site.php
@@ -3,72 +3,78 @@
 title: Site
 pages: default
 fields:
+	title:
+		label: <style>[for$="field-title"],[for$="field-title"] *{display:none;}[for$="field-title"]~*>*{background-color:transparent !important;}[for$="field-title"]~*>.input{border-color:#B3B3B3;}[for$="field-title"]~.field-content{font-size:1.5em;margin-top:2.5rem;}[for$="field-title"]~.field-content .icon{margin-top:2px;transform:translateY(-50%);}</style>
+		type:  text
+		placeholder: Site Title
+		icon: text-width
 	_info-about:
 		type: info
 		text: >
 
-			<div id="about-this-page">
+			<div id="pg_info">
 				<style>
-				#about-this-page h3 {
+				#pg_info {
+					padding: 2.5rem 0 .25em;
+				}
+				#pg_info h3 {
 					position: relative;
 					color: #000;
 					transition: all .3s;
+					cursor: pointer;
+					font-size: 1.1em;
 				}
-				#about-this-page:hover h3 {
+				#pg_info:hover h3 {
 					color: #888;
 				}
-				#about-this-page h3 .icon {
+				#pg_info h3 .icon {
 					position: absolute;
 					right: 100%;
 					height: 1em;
-					bottom: .22em;
+					bottom: .25em;
 					top: initial;
 					margin-right: .375em;
 					color: #AAA;
 					transform: scale(1);
 					transition: all .275s ease-in-out;
-					cursor: pointer;
 				}
-				#about-this-page:hover .icon {
+				#pg_info:hover .icon {
 					/*opacity: 1;*/
 					color: #000;
 					margin-right: .5em;
 					transform: scale(1.25);
 				}
-				#about-this-page section {
+				#pg_info section {
 					max-height: 0;
+					position: absolute;
+					background-color: rgba(37,37,37,.95);
+					border: 1px solid rgba(0,0,0,.75);
+					color: #FCFCFC;
+					padding: .625em;
+					z-index: 9999;
 					pointer-events:none;
 					opacity: 0;
 					overflow: hidden;
 					transition: all .375s ease-in-out;
 				}
-				#about-this-page:hover section {
+				#pg_info:hover section {
 					max-height: 99999999999999999999px;
 					opacity: 1;
 					overflow: initial;
 				}
-				#about-this-page section * { display: none; }
-				#about-this-page:hover section * { display: initial; }
 				</style>
 				<h3>Site Metadata <i class="icon fa fa-question-circle"></i></h3>
 				<section>
 					<br>
-					<p>Think about it as a summary for search engines and social networks that help's them understand your content: it's literally information about information! How else could Google index upwards of 90% of the web?</p>
-					<p>Pertinent and up-to-date metadata can significantly increase your site's relevancy and rankings, as well as user engagement in search results and search social feeds. So get started! Below you can set key search terms, add social network profiles and/or pages, or even upload a thumbnail image for your site.</p>
+					<p>Metadata is information about information. Think of it as a summary of your site. It helps search engines and social networks to understand your site's structure and content. And it helps <em>you</em> increase your rankings and engagement in search results and search social feeds.</p>
+					<p>Pertinent and up-to-date metadata will give you an SEO boost so let's get started! Below you can set key search terms, add social network profiles and/or pages, or even upload a thumbnail image for your site.</p>
 				</section>
-				<hr>
 			</div>
 
 		width: 3/4
-	title:
-		label: <style>[for$="field-title"],[for$="field-title"] *{display:none;}[for$="field-title"]~.field-content{font-size:1.5em;}[for$="field-title"]~.field-content .icon{margin-top:2px;transform:translateY(-50%);}</style>
-		type:  text
-		placeholder: Site Title
-		icon: text-width
 	keywords:
 		label: <small style="font-size:11pt;opacity:.5;">Keywords</small>
 		type:  tags
-		width: 2/4
 	soc_twitter:
 		label: <small style="font-size:11pt;opacity:.5;">Profiles</small>
 		icon: twitter

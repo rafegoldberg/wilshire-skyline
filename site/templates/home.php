475420506005bb4bf8b92d0fd30d0ff635f772c4
Rafe Goldberg <rafegoldberg@gmail.com>
2015-06-19 17:23:04 -0400

    Update hardcoded home

--- site/templates/home.php
+++ site/templates/home.php
@@ -9,7 +9,6 @@
 			</div>
 		</section>
 	-->
-
 	<!-- Hero Box
 		<section class="heroBox">
 			<div class="box--content">
@@ -17,17 +16,29 @@
 			</div>
 		</section>
 	-->
-
 	<!-- Media Box -->
-		<section class="mediaBloc">
-			<div class="imageBox">
+		<section class="attachmentBloc">
+			<div class="mediaBox">
 				<img src="<?=$site->files()->find('s-simplified.png')->url()?>" alt="Mark">
 			</div>
+		</section>
+		<nav class="navBloc">
+			<li class="nav-item">Item 1</li>
+			<li class="nav-item">Item 2</li>
+			<li class="nav-item">Item 3</li>
+			<li class="nav-item">Item 4</li>
+			<li class="nav-item">Item 5</li>
+			<li class="nav-item">Item 6</li>
+		</nav>
+		<section class="textBloc">
 			<div class="contentBox">
-				<h1 class="bit title">Wilshire Skyline</h1>
+				<h1 class="heading_1">Wilshire Skyline</h1>
+			</div>
+			<div class="contentBox centered">
+				<p>Wilshire Skyline manages a portfolio of luxury multi-family residential properties located in prime sub-markets in Los Angeles.</p>
+				<p>In addition to other real estate holdings, the principals of Wilshire Skyline own a controlling interest in all of the properties under management.</p>
+				<p>Currently, Wilshire Skyline manages 18 multi-family residential properties (499 units/ 384,689 square feet) located in Malibu, Santa Monica, Beverly Hills, West Hollywood and Hollywood.</p>
 			</div>
 		</section>
-
-
 	</main>
 <? snippet('meta/post') ?>
